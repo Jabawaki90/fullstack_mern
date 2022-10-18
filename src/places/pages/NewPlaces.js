274e2d664df6f258d97280a8e3b10d9fa22b6958
Ashraf Azemi <ashraf.chzm@gmail.com>
2022-10-18 21:36:19 +0800

    Adding form feature

--- src/places/pages/NewPlaces.js
+++ src/places/pages/NewPlaces.js
@@ -1,7 +1,20 @@
 import React from "react";
 
+import Input from "../../shared/components/FormElements/Input";
+import "./NewPlaces.css";
+
 const NewPlaces = () => {
-  return <h1>New Place Pages</h1>;
+  return (
+    <form className="place-form">
+      <Input
+        element="input"
+        type="text"
+        label="Title"
+        validators={[]}
+        errorText="Please enter a valid title"
+      ></Input>
+    </form>
+  );
 };
 
 export default NewPlaces;
